1dac896c5e98032eaa3d827b3aaccf45083eb520
emanuelguerke <emanuelguerke@users.noreply.github.com>
2019-06-04 00:16:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-// var budgetController = (function(){
-//     var x =23;
-//     var add = function(a){
-//         return x+a;
-//     }
-//     return{
-//         publicTest: function(b) {
-//             return add(b);
-//         }
-//     }
-// })();
-
-// var UIController = (function(){
-//     //some code later
-// })();
-
-// var controller = (function(budgetCtrl, UICtrl){
-//     var z = budgetCtrl.publicTest(5);
-//     return {
-//         anotherPublic: function(){
-//             console.log(z);
-//         }
-//     }
-// })(budgetController, UIController);
-
-
-
 
 //budget controller
 var budgetController = (function(){
